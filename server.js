bb6e6264357a110cbb454c4ac4ef938968597886
emrmason <mas20021@byui.edu>
2024-01-16 13:23:39 -0800

    EJS Templates Activity- Complete, working

--- server.js
+++ server.js
@@ -6,14 +6,27 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+
+/* ***********************
+ * Routes
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+app.get("/", (req, res) => {
+  res.render("index", {title: "home"})
+})
 
 /* ***********************
  * Local Server Information
